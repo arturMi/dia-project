5435d11d0ed0ac1974cd836bc0f6c3e66403a3cf
arturMi <49555233+arturMi@users.noreply.github.com>
2023-12-16 16:54:50 +0100

    next try

--- data_matching.py
+++ data_matching.py
@@ -27,13 +27,13 @@ def calculate_similarities(dataframe_one, dataframe_two, threshold=0.9, column_n
 
 
 # Function to calculate similarities between blocks in two dictionaries
-def calculate_similarities_between_dicts(dict_one, dict_two, threshold=0.9, column_name='Title'):
-    if isinstance(dict_one, dict) and isinstance(dict_two, dict):
+def calculate_similarities_between_dicts(dict_or_dataframe_one, dict_or_dataframe_one_two, threshold=0.9, column_name='Title'):
+    if isinstance(dict_or_dataframe_one, dict) and isinstance(dict_or_dataframe_one_two, dict):
         similarities = []
         
-        for block_name_one, dataframe_one in dict_one.items():
+        for block_name_one, dataframe_one in dict_or_dataframe_one.items():
             if isinstance(dataframe_one, pd.DataFrame):
-                for block_name_two, dataframe_two in dict_two.items():
+                for block_name_two, dataframe_two in dict_or_dataframe_one_two.items():
                     if isinstance(dataframe_two, pd.DataFrame):
                         for idx1, row1 in dataframe_one.iterrows():
                             for idx2, row2 in dataframe_two.iterrows():
@@ -45,7 +45,7 @@ def calculate_similarities_between_dicts(dict_one, dict_two, threshold=0.9, colu
             else:
                 raise ValueError(f"Value for '{block_name_one}' in dict_one is not a DataFrame.")
             
-    elif isinstance(dataframe_one, pd.DataFrame) and isinstance(dataframe_two, pd.DataFrame):
+    elif isinstance(dict_or_dataframe_one, pd.DataFrame) and isinstance(dict_or_dataframe_one_two, pd.DataFrame):
         # If both arguments are DataFrames, proceed with similarity calculations
         similarities = []
         for idx1, row1 in dataframe_one.iterrows():
