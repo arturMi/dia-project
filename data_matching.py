3e841ec2809ff81e8ca353f3c32753b949478e7c
arturMi <49555233+arturMi@users.noreply.github.com>
2023-12-16 16:50:13 +0100

    same

--- data_matching.py
+++ data_matching.py
@@ -45,18 +45,18 @@ def calculate_similarities_between_dicts(dict_one, dict_two, threshold=0.9, colu
         similarities = []
         
         for block_name_one, dataframe_one in dict_one.items():
-            if not isinstance(dataframe_one, pd.DataFrame):
-                raise ValueError("Values in dict_one should be DataFrames.")
-            
-            for block_name_two, dataframe_two in dict_two.items():
-                if not isinstance(dataframe_two, pd.DataFrame):
-                    raise ValueError("Values in dict_two should be DataFrames.")
-                
-                for idx1, row1 in dataframe_one.iterrows():
-                    for idx2, row2 in dataframe_two.iterrows():
-                        sim = jaccard_similarity(tokenize(row1[column_name]), tokenize(row2[column_name]))
-                        if sim >= threshold:
-                            similarities.append({'Block1': block_name_one, 'Block2': block_name_two, 'Similarity': sim})
+            if isinstance(dataframe_one, pd.DataFrame):
+                for block_name_two, dataframe_two in dict_two.items():
+                    if isinstance(dataframe_two, pd.DataFrame):
+                        for idx1, row1 in dataframe_one.iterrows():
+                            for idx2, row2 in dataframe_two.iterrows():
+                                sim = jaccard_similarity(tokenize(row1[column_name]), tokenize(row2[column_name]))
+                                if sim >= threshold:
+                                    similarities.append({'Block1': block_name_one, 'Block2': block_name_two, 'Similarity': sim})
+                    else:
+                        raise ValueError(f"Value for '{block_name_two}' in dict_two is not a DataFrame.")
+            else:
+                raise ValueError(f"Value for '{block_name_one}' in dict_one is not a DataFrame.")
 
         df = pd.DataFrame(similarities)
         return df.to_csv(f'./data/Matched_Entities_Blocks.csv', index=False)
