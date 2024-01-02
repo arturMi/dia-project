d4b8415578b942200872eb2ade91cc29b545596c
arturMi <49555233+arturMi@users.noreply.github.com>
2024-01-02 17:51:53 +0100

    changes

--- data_aquisition.py
+++ data_aquisition.py
@@ -1,10 +1,8 @@
 import os
 import requests
 import tarfile
-import chardet
 
-def download_extract_read(url, file_name):
-    folder_path = "./data/"
+def check_download_extract(url, file_name, folder_path):
     file_path = os.path.join(folder_path, file_name)
 
     if os.path.exists(file_path):
@@ -20,19 +18,7 @@ def download_extract_read(url, file_name):
                 with tarfile.open(file_path, 'r:gz') as tar:
                     tar.extractall(folder_path)
                 print(f"File '{file_name}' downloaded and extracted successfully.")
-
-                # Now read the extracted file with detected encoding
-                extracted_file_path = os.path.join(folder_path, os.path.splitext(file_name)[0])  # Change extension
-                with open(extracted_file_path, 'rb') as extracted_file:
-                    raw_data = extracted_file.read()
-                    detected_encoding = chardet.detect(raw_data)['encoding']
-
-                with open(extracted_file_path, 'r', encoding=detected_encoding) as extracted_file:
-                    data = extracted_file.readlines()
-
-                print("Data read successfully.")
             else:
                 print("Downloaded file is not a .tgz file.")
         else:
             print("Failed to download the file.")
-
